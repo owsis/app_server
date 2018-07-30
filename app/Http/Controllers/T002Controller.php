6895fd44339257f251b2f971cdc64bbb87df2f03
atowsis <atowsis@gmail.com>
2018-07-30 17:03:48 +0700

    30JUL18 VPS 011

--- app/Http/Controllers/T002Controller.php
+++ app/Http/Controllers/T002Controller.php
@@ -114,7 +114,7 @@ class T002Controller extends Controller
     public function login(Request $request, T002 $t002)
     {
 
-        if (!Auth::guard('userapp')->attempt(['phone' => $request->phone, 'password' => $request->password])) {
+        if (!Auth::attempt(['phone' => $request->phone, 'password' => $request->password])) {
             return response()->json(['error' => 'Error'], 404);
         }
         $t002s = $t002->find(Auth::guard('userapp')->user()->id);
