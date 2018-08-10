82dba1ac62aac3a262684daf4d2a7d9467728303
atowsis <atowsis@gmail.com>
2018-08-10 11:21:44 +0700

    10AUG18 VPS 006

--- app/Http/Controllers/T004Controller.php
+++ app/Http/Controllers/T004Controller.php
@@ -13,14 +13,34 @@ class T004Controller extends Controller
     {
         $t006s = T006::where('code_unit', $code)->get();
 
-        $t004s = $t004::where('code_payment', [$t006s->code_payment])
+        $t004s = $t004::where('code_payment', $t006s[0]->code_payment)
+        ->orWhere(function($q) {
+          if ($t006s[1] != null) {
+            $q->where('code_payment', $t006s[1]->code_payment);
+          }
+        })
+        ->orWhere(function($q) {
+          if ($t006s[2] != null) {
+            $q->where('code_payment', $t006s[2]->code_payment);
+          }
+        })
         ->get();
 
         return fractal()
         ->collection($t004s)
         ->transformWith(new T004Transformer)
         ->addMeta([
-            'data_count' => $t004::where('code_payment', [$t006s->code_payment])
+            'data_count' => $t004::where('code_payment', $t006s[0]->code_payment)
+            ->orWhere(function($q) {
+              if ($t006s[1] != null) {
+                $q->where('code_payment', $t006s[1]->code_payment);
+              }
+            })
+            ->orWhere(function($q) {
+              if ($t006s[2] != null) {
+                $q->where('code_payment', $t006s[2]->code_payment);
+              }
+            })
             ->count()
         ])
         ->toArray();
