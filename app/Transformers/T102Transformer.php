8017f5c54352c5b9e63259aec9324004807ed617
owsis <atowsis@gmail.com>
2018-09-08 16:38:56 +0700

    8SEP18 VPS 006

--- app/Transformers/T102Transformer.php
+++ app/Transformers/T102Transformer.php
@@ -18,7 +18,7 @@ class T102Transformer extends TransformerAbstract {
 			'name_user'  => $t102->name_user,
 			'status_key' => $t102->status_key,
 			'status_use' => $t102->status_use,
-			'created_at' => $t102->created_at->format('D, d-M-Y - H:i')
+			'created_at' => $t102->created_at->format('D, d-M-Y H:i:s T')
 		];
 	}
 }
