0ae4e10d4ceb2838c3b57c6fab88263db50c88ad
owsis <atowsis@gmail.com>
2018-09-14 10:55:33 +0700

    14SEP18 002

--- app/Transformers/T011Transformer.php
+++ app/Transformers/T011Transformer.php
@@ -10,9 +10,9 @@ class T011Transformer extends TransformerAbstract
 	public function transform(T011 $t011)
 	{
 		return [
-			'name_img' => $t011->name_img,
-			'type_img' => $t011->type_img,
-			'img1'     => 'http://smile-in-properti.store/storage/app/img_src/' . $t011->img1,
+			'name_unit' => $t011->name_unit,
+			'type_unit' => $t011->type_unit,
+			'img1'     	=> 'http://smile-in-properti.store/storage/app/img_src/' . $t011->img1,
 		];
 	}
 }
